a718752a9bf808935201deca6e5cdad90d8320ea
Mikkel Schmidt Andersen <Mikkel220897@hotmail.com>
2020-01-29 13:59:03 +0100

    This is a very fine test os implemented Accumulator

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -1,32 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Calculator
 {
-   public class Calculator: ICalculator
+    public class Calculator : ICalculator
     {
-      public double Add(double a, double b)
+        public double Accumulator { get; private set; }
+
+        public double Add(double a, double b)
         {
+            Accumulator = a + b;
             return a + b;
         }
 
         public double Subtract(double a, double b)
         {
+            Accumulator = a + b;
             return a - b;
         }
 
         public double Multiply(double a, double b)
         {
+            Accumulator = a + b;
             return a * b;
         }
 
-    
-       public double Power(double x, double exp)
+        public double Power(double x, double exp)
         {
+            Accumulator = Math.Pow(x, exp);
             return Math.Pow(x, exp);
+        }
+
+        public void Clear()
+        {
+            Accumulator = 0;
         }
     }
 }
